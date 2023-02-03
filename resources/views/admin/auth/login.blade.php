9c56d39ea1b9695cd10046a2e2eeaaf56d0f4663
tky0084 <106537623+tky0084@users.noreply.github.com>
2023-02-04 03:26:21 +0900

    resourceコントローラー

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -3,7 +3,7 @@
         アドミン用
         <x-slot name="logo">
             <div class="w-28">
-                <a href="/">
+                <a href="/admin">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
